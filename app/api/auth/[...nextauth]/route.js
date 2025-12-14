588cdede4d6f04b449c5ab95eacea78cc2a8b063
sky138m <eor0423@naver.com>
2025-12-14 22:54:47 +0900

    NextAuth 설정 단순화

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,16 +14,20 @@ const handler = NextAuth({
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/api/auth/signin',
+  },
   callbacks: {
     async redirect({ url, baseUrl }) {
-      // 콜백 URL이 상대경로면 baseUrl과 합치기
+      // 상대경로 처리
       if (url.startsWith("/")) return `${baseUrl}${url}`;
-      // 같은 도메인이면 리다이렉트 허용
+      // 같은 도메인 처리
       else if (new URL(url).origin === baseUrl) return url;
-      // 그 외는 프론트엔드 메인 페이지로
-      return `https://www.zask.kr?loginSuccess=true`;
+      // 프론트엔드로 리다이렉트
+      return `https://www.zask.kr`;
     },
     async session({ session, user }) {
+      // 프론트엔드에서 user.id를 사용할 수 있도록 추가
       if (session?.user) {
         session.user.id = user.id;
       }
